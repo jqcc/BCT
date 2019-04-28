4b85cfa8d8359289d3ab4d240a139c6db6d77ade
Qiang <897238475@qq.com>
2019-04-28 22:41:29 +0800

    测试

---
+++ BlockChainMiner/test_basics.py
@@ -0,0 +1,22 @@
+import unittest
+from blockChainMiner import app
+
+class MinerTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_f1(self):
+        rv = self.app.get('/')
+        assert b'SunJQ Graduation Design - BCT' in rv.data
+
+    def test_f2(self):
+        rv = self.app.get('/chain')
+        print(rv.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
